8e9cc186fafd85b82b5ac378205b7f48093b3f05
AbbyF2291 <abby_freund@hotmail.com>
2021-05-05 16:02:44 +1000

    Fixing Repository
    Fixing up my repository. Error with previous repository where I saved my application

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+
+/*===== API INFO =====*/ 
+
+function fetchDriverStandings(){
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    accept: 'application/json'
+  };
+
+    fetch("https://ergast.com/api/f1/current/driverStandings.json", requestOptions)
+    .then(response => response.json())
+    .then(result => processDriverStandings(result))
+    .catch(error => console.log('error', error));
+}
+
+/*===== FETCH API DATA =====*/ 
+fetchDriverStandings()
+
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== COLLAPSIBLE LIST =====*/
+function addEventListenerToCollapsible(){
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/*===== SCROLL TO TOP =====*/
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
+
+
+
